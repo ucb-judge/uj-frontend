df3ee5a050ce18c53faea67b374948c8d769f252
Radomir Vladislavić <vladislavic.rl@gmail.com>
2023-05-30 23:51:05 -0400

    add features and services templates

---
+++ src/app/features/contests/contests.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContestsRoutingModule } from './contests-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ContestsRoutingModule
+  ]
+})
+export class ContestsModule { }
---
+++ src/app/features/problems/problems.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProblemsRoutingModule } from './problems-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProblemsRoutingModule
+  ]
+})
+export class ProblemsModule { }
